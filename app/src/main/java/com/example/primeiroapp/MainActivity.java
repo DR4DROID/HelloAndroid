96cdfe5b1e417ad99cb3caeaf5af19888b272803
DR4DROID <gilberto.vieira@mg.senac.br>
2022-10-30 12:06:20 -0300

    criado hello  world com mudança de idioma

---
+++ app/src/main/java/com/example/primeiroapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.primeiroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterTexto(View view) {
+        TextView texto = findViewById(R.id.olamundo_id);
+        texto.setText("Olá mundo!");
+    }
+}
